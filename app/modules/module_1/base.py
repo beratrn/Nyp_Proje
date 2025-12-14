ca8cc545b8d67ba94d186b3f63dad9d6db070f06
furkanvanli <furkanvanli355@gmail.com>
2025-12-14 21:51:27 +0300

    ilk commit Nyp

--- app/modules/module_1/base.py
+++ app/modules/module_1/base.py
@@ -1,11 +1,22 @@
 # app/modules/module_1/base.py
+import uuid
+import json
+import datetime
+import inspect
 from abc import ABC, abstractmethod
+# ÖZEL HATA SINIFLARI (EXCEPTION HANDLING)
+class TemelSistemHatasi(Exception):
+    """Sistem genelindeki temel hata sınıfı."""
+    def _init_(self, mesaj):
+        self.mesaj = mesaj
+        self.zaman = datetime.datetime.now()
+        super()._init_(self.mesaj)
 
-class BaseClass1(ABC):
-    def __init__(self, parameter: str):
-        self.base1Attribute = parameter
+class VeriDogrulamaHatasi(TemelSistemHatasi):
+    """Veri tipi veya formatı yanlış olduğunda fırlatılır."""
+    pass
 
-    @abstractmethod
-    def method1(self) -> None:
-        # metot tanimi 2344445
-        pass
+class KimlikHatasi(TemelSistemHatasi):
+    """ID veya benzersizlik çakışmalarında fırlatılır."""
+    pass
+
